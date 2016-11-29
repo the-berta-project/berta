466719fca85b35020e4e491a5702788be6c9cde5
Dusan Baran <dbaran@hotmail.sk>
2016-11-29 12:04:50 +0000

    Add service file

---
+++ lib/berta/service.rb
@@ -0,0 +1,14 @@
+module Berta
+  class Service
+    def initialize(endpoint = 'http://147.251.17.221:2633/RPC2')
+      @endpoint = endpoint
+      @client = OpenNebula::Client.new('oneadmin:opennebula', @endpoint)
+    end
+
+    def running_vms
+      vm_pool = OpenNebula::VirtualMachinePool.new(client) 
+      vm_pool.info_all
+      vm_pool.map { |vm| vm }
+    end
+  end
+end
