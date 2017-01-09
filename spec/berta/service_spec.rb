7d3538b796cc588d312e56256f7f1dc711ed9cd7
Dusan Baran <dbaran@hotmail.sk>
2017-01-09 12:05:47 +0000

    Added cassettes

--- spec/berta/service_spec.rb
+++ spec/berta/service_spec.rb
@@ -15,7 +15,6 @@ describe Berta::Service do
 
   describe '.running_vms' do
     context 'with valid response', :vcr do
-
       it 'gets running vms' do
         vms = service.running_vms
         expect(vms.count).to eq(3)
@@ -25,15 +24,15 @@ describe Berta::Service do
     context 'with wrong secret', :vcr do
       let(:wrong_service) { Berta::Service.new('ahoj:miso', 'http://localhost:2633/RPC2') }
 
-      it 'gets running vms' do
+      it 'raises authentication error' do
         expect { wrong_service.running_vms }.to raise_error Berta::Errors::OpenNebula::AuthenticationError
       end
     end
 
     context 'with wrong endpoint', :vcr do
-      let(:wrong_service) { Berta::Service.new('oneadmin:opennebula', 'http://remotehost/RPC2') }
+      let(:wrong_service) { Berta::Service.new('oneadmin:opennebula', 'http://remotehost:2633/RPC2') }
 
-      it 'gets running vms' do
+      it 'raises resource retrieval error' do
         expect { wrong_service.running_vms }.to raise_error Berta::Errors::OpenNebula::ResourceRetrievalError
       end
     end
