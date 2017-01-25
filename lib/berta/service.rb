511220df1499abeeefd40658d5aba7c2b5933a47
Dusan Baran <dbaran@hotmail.sk>
2017-01-25 08:45:03 +0000

    Improve Service to use whitelist

--- lib/berta/service.rb
+++ lib/berta/service.rb
@@ -24,12 +24,25 @@ module Berta
       vm_pool = OpenNebula::VirtualMachinePool.new(client)
       Berta::Utils::OpenNebula::Helper.handle_error { vm_pool.info_all }
       vm_pool.map { |vm| Berta::VirtualMachineHandler.new(vm) }
+             .keep_if { |vmh| whitelisted?(vmh) && running?(vmh) }
     end
 
+    # Fetch users from OpenNebula
+    #
+    # @return [OpenNebula::UserPool] users on OpenNebula
+    # @raise [Berta::BackendError] if connection failed
     def users
       user_pool = OpenNebula::UserPool.new(client)
       Berta::Utils::OpenNebula::Helper.handle_error { user_pool.info }
       user_pool
+    end
+
+    private
+
+    def whitelisted?(vms)
+    end
+
+    def running?(vms)
     end
   end
 end
