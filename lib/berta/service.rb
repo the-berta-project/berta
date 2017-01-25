c914c4bde34bcb58e01f8126bbec73d4c16adfb0
Dusan Baran <dbaran@hotmail.sk>
2017-01-25 10:18:11 +0000

    Finish whitelist and running

--- lib/berta/service.rb
+++ lib/berta/service.rb
@@ -3,6 +3,8 @@ require 'opennebula'
 module Berta
   # Berta service for communication with OpenNebula
   class Service
+    ACTIVE = 3
+
     attr_reader :endpoint
     attr_reader :client
 
@@ -24,7 +26,7 @@ module Berta
       vm_pool = OpenNebula::VirtualMachinePool.new(client)
       Berta::Utils::OpenNebula::Helper.handle_error { vm_pool.info_all }
       vm_pool.map { |vm| Berta::VirtualMachineHandler.new(vm) }
-             .keep_if { |vmh| whitelisted?(vmh) && running?(vmh) }
+             .delete_if { |vmh| whitelisted?(vmh) || !running?(vmh) }
     end
 
     # Fetch users from OpenNebula
@@ -39,10 +41,39 @@ module Berta
 
     private
 
-    def whitelisted?(vms)
+    def whitelisted?(vmh)
+      whitelisted_id?(vmh) ||
+      whitelisted_user?(vmh) ||
+      whitelisted_group?(vmh) ||
+      whitelisted_cluster?(vmh)
     end
 
-    def running?(vms)
+    def whitelisted_id?(vmh)
+      Berta::Settings.whitelist.ids.find { |id| vmh.handle['ID'] == id } \
+        if vmh.handle['ID'] && Berta::Settings.whitelist.ids
+    end
+
+    def whitelisted_user?(vmh)
+      Berta::Settings.whitelist.users.find { |user| vmh.handle['UID'] == user } \
+        if vmh.handle['UID'] && Berta::Settings.whitelist.users
+    end
+
+    def whitelisted_group?(vmh)
+      Berta::Settings.whitelist.groups.find { |group| vmh.handle['GID'] == group } \
+        if vmh.handle['GID'] && Berta::Settings.whitelist.groups
+    end
+
+    def whitelisted_cluster?(vmh)
+      return unless Berta::Settings.whitelist.clusters
+      cidtime = []
+      vmh.handle.each('HISTORY_RECORDS/HISTORY') \
+        { |history| cidtime << [history['CID'], history['STIME']] }
+      vmcid = cidtime.max { |ct| ct[1] }[0]
+      Berta::Settings.whitelist.clusters.find { |cid| vmcid == cid } if mvcid
+    end
+
+    def running?(vmh)
+      vmh.handle.state == ACTIVE
     end
   end
 end
